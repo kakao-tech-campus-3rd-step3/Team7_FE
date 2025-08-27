e6e0bb76921fc5108be5228d2d00ba9d3db2850b
toothlessdev <toothless042@gmail.com>
2025-08-28 02:48:02 +0900

    feat: NavAside 및 NavAsideItem 컴포넌트 추가

---
+++ src/app/ui/NavAside.tsx
@@ -0,0 +1,41 @@
+import { Link, useLocation } from "react-router";
+
+import { cn } from "@/shared/lib/utils";
+
+export interface NavAsideProps {
+    navAsideMenus: Array<NavAsideItemProps>;
+}
+
+export const NavAside = ({ navAsideMenus }: NavAsideProps) => {
+    return (
+        <aside className="w-[210px] shadow-xs h-full flex-shrink-0 p-4">
+            <nav className="w-full flex flex-col gap-3">
+                {navAsideMenus.map((menu) => {
+                    return <NavAsideItem key={menu.to} {...menu} />;
+                })}
+            </nav>
+        </aside>
+    );
+};
+
+///////////////////////////////////////////////////////////
+
+export interface NavAsideItemProps {
+    label: string;
+    to: string;
+    icon: React.ReactNode;
+}
+
+export const NavAsideItem = ({ icon, label, to }: NavAsideItemProps) => {
+    const location = useLocation();
+    const isActive = location.pathname === to;
+
+    return (
+        <li className={cn("list-none p-2 rounded-sm", isActive && "bg-[#EFF6FF] text-blue-500")}>
+            <Link to={to} className="flex gap-3 items-center">
+                <div>{icon}</div>
+                <p>{label}</p>
+            </Link>
+        </li>
+    );
+};
