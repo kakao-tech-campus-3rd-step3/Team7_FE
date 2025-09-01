5f33e2007622fb533d3ed3abe3f3cd27c36de28d
toothlessdev <toothless042@gmail.com>
2025-09-01 22:38:38 +0900

    feat: NoIndicatorInActive 스토리 추가

--- src/shared/components/Tab/TabNavItem.stories.tsx
+++ src/shared/components/Tab/TabNavItem.stories.tsx
@@ -1,5 +1,6 @@
 import { BookText } from "lucide-react";
 
+import { Tab } from "@/shared/components/Tab/Tab";
 import { TabMenuIndicator } from "@/shared/components/Tab/TabMenuIndicator";
 import { TabNavItem } from "@/shared/components/Tab/TabNavItem";
 
@@ -18,6 +19,13 @@ export const Active: Story = {
         label: "메뉴",
         indicator: <TabMenuIndicator value={1} />,
     },
+    render: (args) => {
+        return (
+            <Tab defaultActiveTab="메뉴">
+                <TabNavItem {...args} />
+            </Tab>
+        );
+    },
 };
 
 export const InActive: Story = {
@@ -26,12 +34,41 @@ export const InActive: Story = {
         label: "메뉴",
         indicator: <TabMenuIndicator value={1} />,
     },
+    render: (args) => {
+        return (
+            <Tab defaultActiveTab="다른메뉴">
+                <TabNavItem {...args} />
+            </Tab>
+        );
+    },
 };
 
-export const NoIndicator: Story = {
+export const NoIndicatorActive: Story = {
     args: {
         icon: <BookText size={16} />,
         label: "메뉴",
         indicator: null,
+    },
+    render: (args) => {
+        return (
+            <Tab defaultActiveTab="메뉴">
+                <TabNavItem {...args} />
+            </Tab>
+        );
+    },
+};
+
+export const NoIndicatorInActive: Story = {
+    args: {
+        icon: <BookText size={16} />,
+        label: "메뉴",
+        indicator: null,
+    },
+    render: (args) => {
+        return (
+            <Tab defaultActiveTab="다른메뉴">
+                <TabNavItem {...args} />
+            </Tab>
+        );
     },
 };
