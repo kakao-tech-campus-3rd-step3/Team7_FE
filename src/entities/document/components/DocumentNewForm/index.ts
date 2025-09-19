34484cf2c539cca189913f24c0c8d2658cd0807d
jiwonWP <bread0264@gmail.com>
2025-09-19 23:11:34 +0900

    chore: 배럴 파일 추가로 컴포넌트 외부 노출 정리

---
+++ src/entities/document/components/DocumentNewForm/index.ts
@@ -0,0 +1,5 @@
+export { DocumentNewForm } from "./DocumentNewForm";
+export type { DocumentNewFormProps } from "./DocumentNewForm";
+
+export { CoverLetterNewForm } from "./CoverLetterNewForm";
+export type { CoverLetterNewFormProps, CoverLetterQuestionItem } from "./CoverLetterNewForm";
