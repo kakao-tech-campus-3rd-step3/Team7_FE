274c0e28f953cbb1d5259ec878428b581213c999
toothlessdev <toothless042@gmail.com>
2025-09-20 06:57:01 +0900

    fix: CommentToolbar 컴포넌트의 nav 요소에 aria-label 속성 추가

--- src/core/document-commenter/components/CommentToolbar/CommentToolbar.tsx
+++ src/core/document-commenter/components/CommentToolbar/CommentToolbar.tsx
@@ -5,7 +5,10 @@ export interface CommentToolbarProps {
 
 export const CommentToolbar = ({ leftItems, rightItems }: CommentToolbarProps) => {
     return (
-        <nav className="bg-[#F9FAFB] w-full h-12 border-gray-200 border-b flex justify-between px-4">
+        <nav
+            className="bg-[#F9FAFB] w-full h-12 border-gray-200 border-b flex justify-between px-4"
+            aria-label="댓글 툴바"
+        >
             <div className="w-full h-full flex items-center justify-start gap-0.5">
                 <ul className="flex gap-1 h-full">{leftItems}</ul>
             </div>
