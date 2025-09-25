890b408a4b55be7ac8cf7e105bb39606cdfc577f
toothlessdev <toothless042@gmail.com>
2025-09-25 23:38:21 +0900

    feat: SelectionEventController 구현

---
+++ src/core/document-commenter/events/SelectionEventController.ts
@@ -0,0 +1,52 @@
+import { EventBus } from "@/core/document-commenter/events/EventBus";
+import { EventController } from "@/core/document-commenter/events/EventController";
+import type { EventHandlerOf } from "@/core/document-commenter/events/EventTypes";
+
+export type SelectionState = "idle" | "dragging";
+
+export class SelectionEventController extends EventController {
+    private eventBus: Nullable<EventBus> = null;
+
+    public state: SelectionState = "idle";
+    public startPosition: Vector2d = { x: 0, y: 0 };
+
+    public override attach(eventBus: EventBus): void {
+        this.eventBus = eventBus;
+
+        this.eventBus.subscribe("document:mousedown", this.handleMouseDown);
+        this.eventBus.subscribe("document:mousemove", this.handleMouseMove);
+        this.eventBus.subscribe("document:mouseup", this.handleMouseUp);
+    }
+
+    private handleMouseDown: EventHandlerOf<"document:mousedown"> = (event) => {
+        if (this.state !== "idle") return;
+
+        this.state = "dragging";
+        this.startPosition = event.payload;
+
+        this.eventBus?.dispatch({
+            type: "selection:start",
+            payload: this.startPosition,
+        });
+    };
+
+    private handleMouseMove: EventHandlerOf<"document:mousemove"> = (event) => {
+        if (this.state !== "dragging") return;
+
+        this.eventBus?.dispatch({
+            type: "selection:move",
+            payload: { start: this.startPosition, current: event.payload },
+        });
+    };
+
+    private handleMouseUp: EventHandlerOf<"document:mouseup"> = (event) => {
+        if (this.state !== "dragging") return;
+
+        this.state = "idle";
+
+        this.eventBus?.dispatch({
+            type: "selection:end",
+            payload: { start: this.startPosition, end: event.payload },
+        });
+    };
+}
