09aea1e74f8e1ab5590916ae52e5984f0d091b61
jiwonWP <bread0264@gmail.com>
2025-09-26 23:33:25 +0900

    chore: index.ts 수정

--- src/entities/document/components/DocumentNewForm/index.ts
+++ src/entities/document/components/DocumentNewForm/index.ts
@@ -1,5 +1,14 @@
+export { CoverLetterNewForm } from "./CoverLetterNewForm";
+export type { CoverLetterNewFormProps } from "./CoverLetterNewForm";
+
+export { CoverLetterQuestionItem } from "./CoverLetterQuestionItem";
+export type {
+    CoverLetterQuestionItemModel,
+    CoverLetterQuestionItemProps,
+} from "./CoverLetterQuestionItem";
+
+export { VersionRadioList } from "./VersionRadioList";
+export type { VersionRadioListProps, VersionRadioItem } from "./VersionRadioList";
+
 export { DocumentNewForm } from "./DocumentNewForm";
 export type { DocumentNewFormProps } from "./DocumentNewForm";
-
-export { CoverLetterNewForm } from "./CoverLetterNewForm";
-export type { CoverLetterNewFormProps, CoverLetterQuestionItem } from "./CoverLetterNewForm";
