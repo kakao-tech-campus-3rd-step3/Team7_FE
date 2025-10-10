4bddf777701cf7568a42800b92dc68cecfc00534
toothlessdev <toothless042@gmail.com>
2025-10-10 12:50:05 +0900

    feat: axios instance 의 interceptor 에  memberId 기본 QueryParams 로 등록 - 백엔드 인증인가 작업 진행 후 JWT 로 변경 예정

--- src/@types/api.d.ts
+++ src/@types/api.d.ts
@@ -3,3 +3,32 @@ declare type BaseResponse<T> = {
     message: string;
     data: T;
 };
+
+declare type BasePaginatedResponse<T> = BaseResponse<{
+    totalElements: number;
+    totalPages: number;
+    pageable: {
+        paged: boolean;
+        pageNumber: number;
+        pageSize: number;
+        offset: number;
+        sort: {
+            sorted: boolean;
+            empty: boolean;
+            unsorted: boolean;
+        };
+        unpaged: boolean;
+    };
+    size: number;
+    content: T;
+    number: number;
+    sort: {
+        sorted: boolean;
+        empty: boolean;
+        unsorted: boolean;
+    };
+    numberOfElements: number;
+    first: boolean;
+    last: boolean;
+    empty: boolean;
+}>;
