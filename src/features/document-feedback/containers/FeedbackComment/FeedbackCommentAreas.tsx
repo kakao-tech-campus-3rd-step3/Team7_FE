f37db38fb2920a7a2e53544b56f79b19c42ce8c7
toothlessdev <toothless042@gmail.com>
2025-10-10 15:16:49 +0900

    feat: 피드백 코멘트 영역 구현 - useGetComments 훅을 사용해 서버로부터 받은 Comment 출력

---
+++ src/features/document-feedback/containers/FeedbackComment/FeedbackCommentAreas.tsx
@@ -0,0 +1,31 @@
+import { useParams } from "react-router";
+
+import { useGetComments } from "@/features/document-feedback/services/getComments";
+
+import { CommentArea } from "@/core/document-commenter/components/Comment";
+
+export const FeedbackCommentAreas = () => {
+    const { id } = useParams();
+    const { data } = useGetComments(Number(id));
+
+    return (
+        <div className="absolute z-50 top-0 left-0">
+            {data.content.map((comment) => {
+                return (
+                    <CommentArea
+                        borderColor="#F6B13B"
+                        backgroundColor="#F6B13B33"
+                        style={{}}
+                        status={"success"}
+                        id={comment.id}
+                        content={comment.content}
+                        startX={comment.coordinate.startX}
+                        startY={comment.coordinate.startY}
+                        endX={comment.coordinate.endX}
+                        endY={comment.coordinate.endY}
+                    />
+                );
+            })}
+        </div>
+    );
+};
