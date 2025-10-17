e289dd30742ce2305a114d88e77d85644d85fa09
hoon_ground <break_the_limit@naver.com>
2025-10-18 02:52:56 +0900

    refactor : mentee-dashboard 배럴파일 경로 수정

---
+++ src/features/mentee-dashboard/index.ts
@@ -0,0 +1,22 @@
+export {
+    DashboardApplyCard,
+    DashboardApplyWrapper,
+    DashboardApplyContainer,
+    DndApplyCard,
+    DndApplySection,
+} from "./components/DashboardApply";
+export { useBoardState } from "./hooks/useBoardState";
+export { sectionState } from "./models/constants";
+export { sectionMock } from "./models/sectionMock";
+export { SECTION_ORDER } from "./models/types";
+export type { Section, ApplyCard } from "./models/types";
+
+export {
+    MenteeDashboardList,
+    MenteeDashboardListContainer,
+    MenteeDashboardListItem,
+} from "./components/MenteeDashboardList";
+export type { MenteeDashboardListItemData } from "./components/MenteeDashboardList";
+export { DashboardViewToggle } from "./components/ViewToggle/DashboardViewToggle";
+export type { DashboardViewMode } from "./components/ViewToggle/DashboardViewToggle";
+export { MenteeDashboardKanban } from "./components/MenteeDashboardKanban/MenteeDashboardKanban";
