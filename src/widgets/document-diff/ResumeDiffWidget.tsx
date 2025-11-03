e917704ecab5577201aa6714ebde7a1bb4642649
jiwonWP <bread0264@gmail.com>
2025-11-03 16:09:33 +0900

    feat: 포트폴리오와 이력서 비교 위젯을 신규 Pdf Diff 구조로 통합

--- src/widgets/document-diff/ResumeDiffWidget.tsx
+++ src/widgets/document-diff/ResumeDiffWidget.tsx
@@ -1,27 +1,8 @@
-import { Fragment } from "react/jsx-runtime";
+import { VersionedPdfDiffWidget, type VersionedDiffWidgetProps } from "./VersionedDiffWidget";
 
-import {
-    VersionNav,
-    VersionNavItem,
-    VersionNavMagnifyController,
-} from "@/features/document-diff/components/VersionNav";
+export interface ResumeDiffWidgetProps
+    extends Omit<VersionedDiffWidgetProps, "titleLeft" | "titleRight"> {}
 
-export const ResumeDiffWidget = () => {
-    return (
-        <Fragment>
-            <VersionNav>
-                <VersionNavItem
-                    variant="original"
-                    label="원본"
-                    controller={<VersionNavMagnifyController />}
-                />
-                <VersionNavItem
-                    variant="modified"
-                    label="수정본"
-                    controller={<VersionNavMagnifyController />}
-                />
-            </VersionNav>
-            이력서 비교!
-        </Fragment>
-    );
+export const ResumeDiffWidget = (props: ResumeDiffWidgetProps) => {
+    return <VersionedPdfDiffWidget titleLeft="원본" titleRight="수정본" {...props} />;
 };
