7f39ae3f68b01aebf65414fdb5b9aebbd7af23e5
hoon_ground <break_the_limit@naver.com>
2025-11-06 14:22:26 +0900

    feat : getCurrentMentorId 함수 추가( (JWT 토큰 연동 전 임시로 mentorId=1 반환)

--- src/shared/lib/auth.ts
+++ src/shared/lib/auth.ts
@@ -5,3 +5,11 @@
 export function getCurrentMemberId(): number {
     return 1;
 }
+
+// 현재 로그인한 멘토의 mentorId 반환
+// TODO: 추후 JWT 토큰에서 실제 mentorId를 가져오도록 수정하기
+// return : 현재 멘토 mentorId
+
+export function getCurrentMentorId(): number {
+    return 1;
+}
