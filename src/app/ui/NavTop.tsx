a1b0dc3f45bc009d8a6c9c81c241e49287063ba7
toothlessdev <toothless042@gmail.com>
2025-11-07 07:44:05 +0900

    feat: NavTop 컴포넌트에 인증 상태에 따른 버튼 변경 기능 추가

--- src/app/ui/NavTop.tsx
+++ src/app/ui/NavTop.tsx
@@ -1,10 +1,17 @@
+import { useNavigate } from "react-router";
+
 import logo from "@/app/assets/logo.svg";
+
+import { useAuthStore } from "@/features/authentication/store/authStore";
 
 import { Button } from "@/shared/ui/button";
 
 export const NAV_TOP_HEIGHT = 64;
 
 export const NavTop = () => {
+    const navigate = useNavigate();
+    const { isAuthenticated, clearTokens } = useAuthStore();
+
     return (
         <nav className="w-full shadow-xs" style={{ height: NAV_TOP_HEIGHT }}>
             <ul className="w-full max-w-[1200px] mx-auto h-full px-4 flex items-center justify-between">
@@ -12,7 +19,25 @@ export const NavTop = () => {
                     <img src={logo} alt="CareerFit 커리어핏" />
                 </li>
                 <li>
-                    <Button>로그인</Button>
+                    {isAuthenticated ? (
+                        <Button
+                            variant="destructive"
+                            onClick={() => {
+                                navigate("/");
+                                clearTokens();
+                            }}
+                        >
+                            로그아웃
+                        </Button>
+                    ) : (
+                        <Button
+                            onClick={() => {
+                                navigate("/auth/signin");
+                            }}
+                        >
+                            로그인
+                        </Button>
+                    )}
                 </li>
             </ul>
         </nav>
