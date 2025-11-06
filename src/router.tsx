d06eb4a9bee407a41623d6c0ebef72903a5c0864
jiwonWP <bread0264@gmail.com>
2025-11-07 04:17:21 +0900

    feat: 새 문서 조회가 ID값을 필요로해 일부 수정

--- src/router.tsx
+++ src/router.tsx
@@ -37,16 +37,14 @@ const routes = createRoutesFromElements(
                 <Route path="search" element={<MenteeSearchMentorPage />} />
                 <Route path="settings" element={<MenteeSettingsPage />} />
 
-                <Route path="applications"></Route>
-                <Route path="resumes/new" element={<NewResumePage />} />
-                <Route path="coverletters/new" element={<NewCoverLetterPage />} />
-                <Route path="portfolios/new" element={<NewPortfolioPage />} />
-
                 <Route path="applications/:applicationId">
                     <Route index></Route>
                     <Route path="resumes" element={<ApplicationDetailPage />} />
                     <Route path="coverletters" element={<ApplicationDetailPage />} />
                     <Route path="portfolios" element={<ApplicationDetailPage />} />
+                    <Route path="resumes/new" element={<NewResumePage />} />
+                    <Route path="coverletters/new" element={<NewCoverLetterPage />} />
+                    <Route path="portfolios/new" element={<NewPortfolioPage />} />
 
                     <Route path="diff" element={<DocumentDiffPage />} />
                 </Route>
