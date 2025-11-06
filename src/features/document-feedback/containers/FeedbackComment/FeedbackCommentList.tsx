ed2345f276060affac88e011e416278ba8f71553
toothlessdev <toothless042@gmail.com>
2025-11-07 03:43:26 +0900

    feat: FeedbackCommentList 컴포넌트 추가 및 댓글 목록 표시 기능 구현

---
+++ src/features/document-feedback/containers/FeedbackComment/FeedbackCommentList.tsx
@@ -0,0 +1,24 @@
+import { useParams } from "react-router";
+
+import { FeedbackComment } from "@/features/document-feedback/components/FeedbackComment/FeedbackComment";
+import { useGetComments } from "@/features/document-feedback/services/getComments";
+
+export const FeedbackCommentList = () => {
+    const { id } = useParams();
+    const { data } = useGetComments(Number(id));
+
+    return (
+        <ul className="flex flex-col gap-2 overflow-y-scroll">
+            {data.map((comment) => {
+                return (
+                    <FeedbackComment
+                        id={comment.id}
+                        author={comment.writerInfo.name}
+                        content={comment.content}
+                        createdAt={"2023-10-01T12:00:00Z"}
+                    />
+                );
+            })}
+        </ul>
+    );
+};
